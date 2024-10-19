811cb9681904f332a2229d7e8d050c37d4df42e6
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-10-19 22:13:20 +0600

    feat(client): Add category selection and dynamic data fetching

--- src/components/Categories/CategoryBox.jsx
+++ src/components/Categories/CategoryBox.jsx
@@ -1,8 +1,25 @@
 import PropTypes from 'prop-types'
+import queryString from 'query-string';
+import { useNavigate } from 'react-router-dom';
 
 const CategoryBox = ({ label, icon: Icon }) => {
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    const currentQuery = { category: label }; 
+
+    const URL = queryString.stringifyUrl({
+      url: "/",
+      query: currentQuery
+    });
+
+    navigate(URL)
+  };
+
   return (
     <div
+    onClick={handleClick}
       className={`flex 
   flex-col 
   items-center 
