08ee0280eaf208226c90f5a3763174aaedf5a54f
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-10-22 20:01:47 +0600

    adrroom form date range

--- src/pages/Dashboard/Host/Addroom.jsx
+++ src/pages/Dashboard/Host/Addroom.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import AddRoomForm from "../../../components/form/AddRoomForm";
 
 
 const Addroom = () => {
+    const [dates, setDates] = useState(
+        {
+            startDate: new Date(),
+            endDate: null,
+            key: 'selection'
+        }
+    );
+
+    const handleDates = range => {
+        console.log(range);
+        setDates(range.selection)
+    }
     return (
         <div>
             <p>This is add room</p>
 
             {/* form */}
-            <AddRoomForm></AddRoomForm>
+            <AddRoomForm dates={dates} handleDates={handleDates}></AddRoomForm>
         </div>
     );
 };
