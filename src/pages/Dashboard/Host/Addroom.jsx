121f2b47626bf9fe6374733ad93996b067af0646
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-10-22 15:18:11 +0600

    date range in addrom form

--- src/pages/Dashboard/Host/Addroom.jsx
+++ src/pages/Dashboard/Host/Addroom.jsx
@@ -1,9 +1,13 @@
+import AddRoomForm from "../../../components/form/AddRoomForm";
 
 
 const Addroom = () => {
     return (
         <div>
             <p>This is add room</p>
+
+            {/* form */}
+            <AddRoomForm></AddRoomForm>
         </div>
     );
 };
