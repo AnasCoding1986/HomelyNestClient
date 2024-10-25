26355015aa98a5e4f629622695054520e9eca03c
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-10-25 07:58:42 +0600

    my listng data fetching successfull

--- src/pages/Dashboard/Host/Mylistings.jsx
+++ src/pages/Dashboard/Host/Mylistings.jsx
@@ -1,6 +1,27 @@
+import { useQuery } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
+import useAxiosSecure from '../../../hooks/useAxiosSecure'
+import useAuth from '../../../hooks/useAuth'
+import RoomDataRow from '../../../components/Dshboard/tableRow/RoomdataRow'
 
 const MyListings = () => {
+
+    const {user} = useAuth();
+    const axiosSecure = useAxiosSecure();
+
+    const { data: rooms = [], isLoading, error,refetch } = useQuery({
+      queryKey: ['rooms',user?.email], // Assign a proper query key
+      queryFn: async () => {
+        const { data } = await axiosSecure.get(`/my-listings/${user?.email}`); // Using axiosCommon instance to make the request
+        return data;
+      }
+    });
+
+    console.log(rooms);
+  
+    if (isLoading) return <p>Loading...</p>;
+    if (error) return <p>Error fetching data</p>;
+
   return (
     <>
       <Helmet>
@@ -58,7 +79,11 @@ const MyListings = () => {
                     </th>
                   </tr>
                 </thead>
-                <tbody>{/* Room row data */}</tbody>
+                <tbody>
+                    {/* Room row data */
+                    rooms?.map(room=> <RoomDataRow key={room._id} room={room} refetch={refetch}></RoomDataRow>)
+                    }
+                </tbody>
               </table>
             </div>
           </div>
