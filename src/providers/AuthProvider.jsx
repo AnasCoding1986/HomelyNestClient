cc35370ce87eefd5dbfdf46de3026c3e1f126b83
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-10-29 07:49:55 +0600

    user saved successfully

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -13,6 +13,7 @@ import {
 } from 'firebase/auth'
 import { app } from '../firebase/firebase.config'
 import axios from 'axios'
+import toast from 'react-hot-toast'
 export const AuthContext = createContext(null)
 const auth = getAuth(app)
 const googleProvider = new GoogleAuthProvider()
@@ -67,16 +68,30 @@ const AuthProvider = ({ children }) => {
 
   // SaveUser
   const saveUser = async (user) => {
-    const {data} = await axios.put("/user");
-    return data
-  }
+    const cUser = {
+      email: user?.email,
+      role: "Guest",
+      status: "verified",
+    };
+  
+    try {
+      const { data } = await axios.put(`${import.meta.env.VITE_API_URL}/user`, cUser);
+      toast.success("user saved");
+      console.log("User saved successfully", data);
+      return data;
+    } catch (error) {
+      console.error("Error saving user:", error);
+      toast.error("user not saved");
+    }
+  };  
 
   // onAuthStateChange
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, currentUser => {
       setUser(currentUser)
       if (currentUser) {
-        getToken(currentUser.email)
+        getToken(currentUser.email);
+        saveUser(currentUser)
       }
       setLoading(false)
     })
